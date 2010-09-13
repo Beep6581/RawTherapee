a5d9a88f11f621b627c43ab053e08b4a73cc1a1b
Andrey Skvortsov <Andrey Skvortsov@localhost>
2010-09-12 17:09:04 -0700

    Bilinear demosaic extremely fast and simple.

---
+++ rtengine/dcrop.h
@@ -0,0 +1,74 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _CROP_H_
+#define _CROP_H_
+
+#include <improccoordinator.h>
+#include <rtengine.h>
+#include <improcfun.h>
+#include <image8.h>
+#include <image16.h>
+#include <imagesource.h>
+#include <procevents.h>
+
+namespace rtengine {
+
+using namespace procparams;
+
+class ImProcCoordinator;
+
+class Crop : public DetailedCrop {
+
+    protected:
+        Image16* origCrop, *resizeCrop, *transCrop, *baseCrop;
+        LabImage *laboCrop, *labnCrop;
+        Image8 *cropImg;
+
+        int** cbuffer;
+        SHMap* cshmap;
+
+        bool updating, needsNext;
+        int skip;
+        int cropx, cropy, cropw, croph;         // size of the detail crop image ('skip' taken into account), with border
+        int trafx, trafy, trafw, trafh;         // the size and position to get from the imagesource that is transformed to the requested crop area
+        int rqcropx, rqcropy, rqcropw, rqcroph; // size of the requested detail crop image (the image might be smaller) (without border)
+        int borderRequested, upperBorder, leftBorder;
+        
+
+        bool cropAllocated;
+        DetailedCropListener* cropImageListener;
+        Glib::Mutex cropMutex;
+        ImProcCoordinator* parent;
+
+        bool setCropSizes (int cx, int cy, int cw, int ch, int skip, bool internal);
+        void freeAll ();
+        
+    public:
+             Crop        (ImProcCoordinator* parent);
+            ~Crop        ();
+    
+        bool hasListener () { return cropImageListener; }
+        void update      (int todo, bool internal=false);
+        void setWindow   (int cx, int cy, int cw, int ch, int skip) { setCropSizes (cx, cy, cw, ch, skip, false); }
+        void fullUpdate  ();
+        void setListener (DetailedCropListener* il);
+        void destroy     () { delete this; }
+};
+}
+#endif
---
+++ rtgui/curveeditor.h
@@ -0,0 +1,85 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _CURVEEDITOR_
+#define _CURVEEDITOR_
+
+#include <gtkmm.h>
+#include <mycurve.h>
+#include <shcselector.h>
+#include <adjuster.h>
+
+class CurveEditor : public Gtk::VBox, public CurveListener, public SHCListener, public AdjusterListener {
+
+        Gtk::ComboBoxText* curveType;
+        Gtk::Button* curve_reset;
+        Gtk::VBox* paramCurveBox;
+        Gtk::VBox* customCurveBox;
+        Gtk::VBox* NURBSCurveBox;
+
+        MyCurve* customCurve;
+        MyCurve* NURBSCurve;
+        MyCurve* paramCurve;
+        SHCSelector* shcSelector;
+
+        Adjuster* highlights;
+        Adjuster* lights;
+        Adjuster* darks;
+        Adjuster* shadows;
+
+        Gtk::Button* saveCustom;
+        Gtk::Button* loadCustom;
+        Gtk::Button* saveNURBS;
+        Gtk::Button* loadNURBS;
+
+        CurveListener* cl;
+
+        bool realized;
+        std::vector<double> tmpCurve;
+        CurveType curveTypeIx;
+
+        int activeParamControl;
+
+        sigc::connection typeconn;
+
+    public:
+
+        CurveEditor ();
+        virtual ~CurveEditor ();
+        void setBatchMode (bool batchMode);
+        bool isUnChanged ();
+        void setUnChanged (bool uc);
+        
+        void on_realize ();
+        void setCurveListener (CurveListener* l) { cl = l; }
+        void savePressed ();
+        void loadPressed ();
+        void typeSelectionChanged ();
+        void setCurve (const std::vector<double>& c);
+        std::vector<double> getCurve ();
+        void curveChanged ();
+        void curveResetPressed ();
+        void shcChanged ();
+        void adjusterChanged (Adjuster* a, double newval);
+        bool adjusterEntered (GdkEventCrossing* ev, int ac);
+        bool adjusterLeft (GdkEventCrossing* ev, int ac);
+        void updateBackgroundHistogram (unsigned int* hist);
+};
+
+
+#endif
