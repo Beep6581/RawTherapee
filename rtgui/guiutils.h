d3714bf3d4883259870ee949230a246c62e88a79
Wyatt Olson <wyatt.olson@gmail.com>
2010-10-14 09:14:32 -0600

    Updated Swedish on behalf of Johan Andersson; re-generated other locales to include new translation keys

---
+++ rtgui/guiutils.h
@@ -0,0 +1,31 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __UTILS_
+#define __UTILS_
+
+#include <gtkmm.h>
+#include <rtengine.h>
+
+bool removeIfThere (Gtk::Container* cont, Gtk::Widget* w, bool increference=true);
+void thumbInterp (const unsigned char* src, int sw, int sh, unsigned char* dst, int dw, int dh);
+Glib::ustring removeExtension (const Glib::ustring& filename);
+Glib::ustring getExtension (const Glib::ustring& filename);
+void drawCrop (Cairo::RefPtr<Cairo::Context> cr, int imx, int imy, int imw, int imh, int startx, int starty, double scale, const rtengine::procparams::CropParams& cparams);
+
+#endif
