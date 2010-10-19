3715bf42e3d6acd2b0f5fe2e630dcab49033aa7f
Emil Martinec <ejmartin@uchicago.edu>
2010-10-19 01:10:54 -0500

    Bugfixes for tone curves.

--- rtgui/tonecurve.cc
+++ rtgui/tonecurve.cc
@@ -55,7 +55,7 @@ ToneCurve::ToneCurve () : ToolPanel(), expAdd(false), blackAdd(false), brAdd(fal
   pack_start (*hlcompr);
 
 //----------- Black Level ----------------------------------
-  black = Gtk::manage (new Adjuster (M("TP_EXPOSURE_BLACKLEVEL"), 0, 32768, 1, 0));
+  black = Gtk::manage (new Adjuster (M("TP_EXPOSURE_BLACKLEVEL"), 0, 16384, 1, 0));
   pack_start (*black);
   shcompr = Gtk::manage (new Adjuster (M("TP_EXPOSURE_COMPRSHADOWS"), -100, 100, 1, 0));
   pack_start (*shcompr);
