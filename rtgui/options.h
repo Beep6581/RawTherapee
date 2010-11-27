b889b0de3fbc188c5fc51e712fdb03a8b370d9bb
Oliver Duis <oduis@hotmail.com>
2010-11-27 15:03:52 +0100

    Added option for crop area color/opacy; See issue #355

--- rtgui/options.h
+++ rtgui/options.h
@@ -133,9 +133,9 @@ class Options {
     bool tabbedUI;
     int previewSizeTab,previewSizeBrowser;
     int multiDisplayMode;  // 0=none, 1=Edit panels on other display
+    std::vector<double> cutOverlayBrush;  // Red;Green;Blue;Alpha , all ranging 0..1
     
-    
-                Options         ();
+    Options ();
 
     Options*    copyFrom        (Options* other);
     void        setDefaults     ();
