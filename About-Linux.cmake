7c561d426fd3c6708a83332034852cd8bb36ac72
Hombre <natureh@free.fr>
2011-02-08 16:58:56 +0100

    Bug correction in the version handling mechanism, adding the "Bit depth" parameter and promoting Michael Erza as developer in Authors.txt

--- About-Linux.cmake
+++ About-Linux.cmake
@@ -7,11 +7,15 @@ set (OUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/AboutThisBuild.txt")
 set (VERSION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/rtgui/version.h")
 set (SHELL "/bin/bash")
 
+# if AboutThisBuild.txt has to be deleted, i guess that it's also the case for version.txt...
 add_custom_target (AboutFile ALL
+	COMMAND rm -f ${OUT_FILE}
+	COMMAND rm -f ${VERSION_FILE}
 	COMMAND for i in \$\( hg branch \)\; do echo Branch: $i >${OUT_FILE}\; done
 	COMMAND hg parents --template=\"Version: {latesttag}_{latesttagdistance}\\nChangset: {node}\\n\" >>${OUT_FILE}
 	COMMAND for i in \$\( gcc -dumpversion \) \;do echo Compiler: GCC $i >>${OUT_FILE} \; done
 	COMMAND echo Processor: ${PROC_LABEL} >>${OUT_FILE}
+	COMMAND echo Bit depth: ${PROC_BIT_DEPTH} >>${OUT_FILE}
 	COMMAND echo Gtkmm: V${GTKMM_VERSION} >>${OUT_FILE}
 	COMMAND echo Build flags: ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${UPPER_CMAKE_BUILD_TYPE}} >>${OUT_FILE}
 	COMMAND echo Link flags: ${CMAKE_EXE_LINKER_FLAGS} ${CMAKE_EXE_LINKER_FLAGS_${UPPER_CMAKE_BUILD_TYPE}}  >>${OUT_FILE}
