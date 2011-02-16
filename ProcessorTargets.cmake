ab708b9f78cd8919dbbf0005fb4e5969bf576c00
Hombre <natureh@free.fr>
2011-02-17 00:56:52 +0100

    Removing a compilation flag that make RT crash in some circumstance (maybe because of -O2 in the Release target)

--- ProcessorTargets.cmake
+++ ProcessorTargets.cmake
@@ -11,23 +11,23 @@ set(PROC_TARGET_2_FLAGS "-march=native" CACHE STRING "Processor-2 flags")
 # The later choices is intended to be used if you want to provide specific builds, but it should match your own processor
 # You can cross compile but you have to know what you're doing, this mechanism has not been designed for that
 
-set(PROC_TARGET_3_LABEL pentium/no-sse CACHE STRING "Processor-3 label - use it to provide a pentium/no sse optimized build, if you have this processor")
-set(PROC_TARGET_3_FLAGS "-march=pentium -mfpmath=387" CACHE STRING "Processor-3 flags")
+set(PROC_TARGET_3_LABEL pentium CACHE STRING "Processor-3 label - use it to provide a pentium optimized build, if you have this processor")
+set(PROC_TARGET_3_FLAGS "-march=pentium" CACHE STRING "Processor-3 flags")
 
 set(PROC_TARGET_4_LABEL pentium4 CACHE STRING "Processor-4 label - use it to provide a pentium4 optimized build, if you have this processor")
-set(PROC_TARGET_4_FLAGS "-march=pentium4 -mfpmath=sse" CACHE STRING "Processor-4 flags")
+set(PROC_TARGET_4_FLAGS "-march=pentium4" CACHE STRING "Processor-4 flags")
 
 set(PROC_TARGET_5_LABEL core2 CACHE STRING "Processor-5 label - use it to provide a core2 optimized build, if you have this processor")
-set(PROC_TARGET_5_FLAGS "-march=core2 -mfpmath=sse" CACHE STRING "Processor-5 flags")
+set(PROC_TARGET_5_FLAGS "-march=core2" CACHE STRING "Processor-5 flags")
 
 set(PROC_TARGET_6_LABEL corei7 CACHE STRING "Processor-6 label - use it to provide a corei7 optimized build, if you have this processor")
-set(PROC_TARGET_6_FLAGS "-march=corei7 -mfpmath=sse" CACHE STRING "Processor-6 flags")
+set(PROC_TARGET_6_FLAGS "-march=corei7" CACHE STRING "Processor-6 flags")
 
 set(PROC_TARGET_7_LABEL athlon-4 CACHE STRING "Processor-7 label - use it to provide a athlon-4 optimized build, if you have this processor")
-set(PROC_TARGET_7_FLAGS "-march=athlon-4 -mfpmath=sse" CACHE STRING "Processor-7 flags")
+set(PROC_TARGET_7_FLAGS "-march=athlon-4" CACHE STRING "Processor-7 flags")
 
 set(PROC_TARGET_8_LABEL athlon64 CACHE STRING "Processor-8 label - use it to provide a athlon64 optimized build, if you have this processor")
-set(PROC_TARGET_8_FLAGS "-march=pentium2 -mtune=athlon64 -mfpmath=sse" CACHE STRING "Processor-8 flags")
+set(PROC_TARGET_8_FLAGS "-march=pentium2 -mtune=athlon64" CACHE STRING "Processor-8 flags")
 
 #set(PROC_TARGET__LABEL procLabel CACHE STRING "Processor- label")
 #set(PROC_TARGET__FLAGS "procFlags" CACHE STRING "Processor- flags")
