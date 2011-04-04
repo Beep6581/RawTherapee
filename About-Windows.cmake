f64720efced3de4279a248d00cfeb000490b3af3
Hombre <natureh@free.fr>
2011-04-05 01:35:27 +0200

    Typo correction in AboutThisBuild.txt : changset -> changeset

--- About-Windows.cmake
+++ About-Windows.cmake
@@ -15,7 +15,7 @@ configure_file (${CMAKE_CURRENT_SOURCE_DIR}/rtgui/version.h.in ${CMAKE_CURRENT_B
 add_custom_target (AboutFile ALL
 	COMMAND for /F \"tokens=*\" %%i in \('hg -R \"${CMAKE_CURRENT_SOURCE_DIR}\" branch'\) do echo Branch: %%i >${OUT_FILE}
 	COMMAND echo Version: ${HG_VERSION} >>${OUT_FILE}
-	COMMAND echo Changset: ${HG_CHANGESET} >>${OUT_FILE}
+	COMMAND echo Changeset: ${HG_CHANGESET} >>${OUT_FILE}
 	COMMAND for /F \"tokens=*\" %%i in \('gcc -dumpversion'\) do echo Compiler: GCC%%i >>${OUT_FILE}
 	COMMAND \(echo Processor: ${PROC_LABEL}\) >>${OUT_FILE}
 	COMMAND \(echo Bit depth: ${PROC_BIT_DEPTH}\) >>${OUT_FILE}
