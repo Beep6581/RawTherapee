5c718d72e81be96da4d4c855c7dfbfb6e4f349ea
natureh <natureh@free.fr>
2011-04-10 13:17:22 +0200

    Reverting erroneous commit

--- About-Windows.cmake
+++ About-Windows.cmake
@@ -10,12 +10,12 @@ execute_process(COMMAND hg parents --template={node|short} WORKING_DIRECTORY ${C
 execute_process(COMMAND hg parents --template={latesttagdistance} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} OUTPUT_VARIABLE HG_TAGDISTANCE)
 
 # build version.h from template
-configure_file (${CMAKE_CURRENT_SOURCE_DIR}/rtgui/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/rtgui/version.h)
+configure_file (${CMAKE_CURRENT_SOURCE_DIR}/rtgui/version.h.in ${CMAKE_CURRENT_SOURCE_DIR}/rtgui/version.h)
 
 add_custom_target (AboutFile ALL
 	COMMAND for /F \"tokens=*\" %%i in \('hg -R \"${CMAKE_CURRENT_SOURCE_DIR}\" branch'\) do echo Branch: %%i >${OUT_FILE}
 	COMMAND echo Version: ${HG_VERSION} >>${OUT_FILE}
-	COMMAND echo Changset: ${HG_CHANGESET} >>${OUT_FILE}
+	COMMAND echo Changeset: ${HG_CHANGESET} >>${OUT_FILE}
 	COMMAND for /F \"tokens=*\" %%i in \('gcc -dumpversion'\) do echo Compiler: GCC%%i >>${OUT_FILE}
 	COMMAND \(echo Processor: ${PROC_LABEL}\) >>${OUT_FILE}
 	COMMAND \(echo Bit depth: ${PROC_BIT_DEPTH}\) >>${OUT_FILE}
