cbdbbc8a5932906d2697b331d0bb303a70a2c810
torger <torger@ludd.ltu.se>
2014-10-29 10:27:46 +0100

    Clang compile fix in DNG float code

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -8725,7 +8725,8 @@ static void decodeFPDeltaRow(Bytef * src, Bytef * dst, size_t tileWidth, size_t 
       dst[col*3 + 2] = src[col + realTileWidth*2];
     }
   } else {
-    if (((union { uint32_t x; uint8_t c; }){1}).c) {
+    union X { uint32_t x; uint8_t c; };
+    if (((union X){1}).c) {
 		for (size_t col = 0; col < tileWidth; ++col) {
 			for (size_t byte = 0; byte < bytesps; ++byte)
 				dst[col*bytesps + byte] = src[col + realTileWidth*(bytesps-byte-1)];  // Little endian
