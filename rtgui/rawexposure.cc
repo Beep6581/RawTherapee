7913f9177329a87678a99ec061f81f47e92c8e92
torger <torger@ludd.ltu.se>
2015-07-10 12:00:36 +0200

    Issue 2134: removed obsolete raw highlight preservation setting from GUI (still left in procparams for backwards compatilibility)

--- rtgui/rawexposure.cc
+++ rtgui/rawexposure.cc
@@ -36,7 +36,8 @@ RAWExposure::RAWExposure () : FoldableToolPanel(this, "rawexposure", M("TP_EXPOS
 	PexPreser->show();
 
 	pack_start( *PexPos, Gtk::PACK_SHRINK, 4);//exposi
-	pack_start( *PexPreser, Gtk::PACK_SHRINK, 4);
+        // raw highlight exposure setting is obsolete, removing from GUI
+	//pack_start( *PexPreser, Gtk::PACK_SHRINK, 4);
 }
 
 void RAWExposure::read(const rtengine::procparams::ProcParams* pp, const ParamsEdited* pedited)
