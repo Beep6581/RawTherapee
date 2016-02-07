00c22ef95e7a8275b96d406d1692bc8c3396998c
Adam Reichold <adam.reichold@t-online.de>
2016-02-07 18:11:24 +0100

    Fix incorrect comment and add missing preamble to jagged array header.

--- rtengine/jaggedarray.h
+++ rtengine/jaggedarray.h
@@ -1,10 +1,29 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2016 Ingo Weyrich <heckflosse67@gmx.de>
+ *  Copyright (c) 2016 Adam Reichold <adam.reichold@t-online.de>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef JAGGEDARRAY_H
 #define JAGGEDARRAY_H
 
 namespace rtengine
 {
 
-// These emulate a jagged array, but use only 2 allocations instead of 1 + W.
+// These emulate a jagged array, but use only 2 allocations instead of 1 + H.
 
 template<class T>
 inline T** const allocJaggedArray (const int W, const int H, const bool initZero = false)
