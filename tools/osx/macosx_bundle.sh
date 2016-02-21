9f29f49f0196ae3bec1dd54971a9d6f221372e36
Beep6581 <entertheyoni@gmail.com>
2016-02-21 23:45:48 +0100

    Re-formatted tools/osx/macosx_bundle.sh

--- tools/osx/macosx_bundle.sh
+++ tools/osx/macosx_bundle.sh
@@ -11,24 +11,24 @@
 # - GTK_PREFIX
 
 function message {
-  printf '\e[34m-- %s\e[m\n' "$*"
+    printf '\e[34m-- %s\e[m\n' "$*"
 }
 
 function GetDependencies {
-  otool -L "$1" | awk 'NR >= 2 && $1 !~ /^(\/usr\/lib|\/System|@executable_path|@rpath)\// { print $1 }'
+    otool -L "$1" | awk 'NR >= 2 && $1 !~ /^(\/usr\/lib|\/System|@executable_path|@rpath)\// { print $1 }'
 }
 
 function CheckLink {
-  GetDependencies "$1" | while read; do
-    local dest="${LIB}/$(basename "${REPLY}")"
-    test -f "${dest}" || { ditto --arch ${arch} "${REPLY}" "${dest}"; CheckLink "${dest}"; }
-  done
+    GetDependencies "$1" | while read; do
+        local dest="${LIB}/$(basename "${REPLY}")"
+        test -f "${dest}" || { ditto --arch ${arch} "${REPLY}" "${dest}"; CheckLink "${dest}"; }
+    done
 }
 
 # source check
 if test ! -d "${CMAKE_BUILD_TYPE}"; then
-  printf "\e[31m${PWD}/${CMAKE_BUILD_TYPE} directory is not found. Please execute 'make install' first.\e[m\n"
-  exit 1
+    printf "\e[31m${PWD}/${CMAKE_BUILD_TYPE} directory is not found. Please execute 'make install' first.\e[m\n"
+    exit 1
 fi
 
 # update project version
@@ -45,7 +45,7 @@ fi
 # 'MINIMUM_VERSION' will be used host OS X version.
 MINIMUM_SYSTEM_VERSION="$(otool -l "${CMAKE_BUILD_TYPE}"/rawtherapee | grep -A2 'LC_VERSION_MIN_MACOSX' | awk '$1 ~ /version/ { printf $2 }')"
 if test ! -n "${MINIMUM_SYSTEM_VERSION}"; then
-  MINIMUM_SYSTEM_VERSION="$(sw_vers -productVersion | cut -d. -f-2)"
+    MINIMUM_SYSTEM_VERSION="$(sw_vers -productVersion | cut -d. -f-2)"
 fi
 
 # check for pango-querymodules. Pango 1.38.0 and above do not include it.
@@ -61,8 +61,8 @@ else
 fi
 
 case ${PROC_BIT_DEPTH} in
-  64) arch=x86_64;;
-  32) arch=i386;;
+    64) arch=x86_64;;
+    32) arch=i386;;
 esac
 
 cat <<__EOS__
@@ -89,9 +89,9 @@ rm -rf "${APP}" ${PROJECT_NAME}_*.dmg
 
 message "Creating bundle container"
 install -d  "${RESOURCES}" \
-            "${MACOS}" \
-            "${LIB}" \
-            "${ETC}"
+"${MACOS}" \
+"${LIB}" \
+"${ETC}"
 
 message "Copying release files"
 ditto "${CMAKE_BUILD_TYPE}" "${MACOS}"
@@ -113,7 +113,7 @@ cp "${GTK_PREFIX}"/etc/gtk-2.0/im-multipress.conf "${ETC}"/gtk-2.0
 "${GTK_PREFIX}"/bin/gdk-pixbuf-query-loaders "${LIB}"/gdk-pixbuf-2.0/*/loaders/*.so > "${ETC}"/gtk-2.0/gdk-pixbuf.loaders
 "${GTK_PREFIX}"/bin/gtk-query-immodules-2.0  "${LIB}"/gtk-2.0/*/immodules/*.so      > "${ETC}"/gtk-2.0/gtk.immodules
 sed -i "" -e "s|${PWD}|/tmp|" "${ETC}"/gtk-2.0/gdk-pixbuf.loaders \
-                              "${ETC}"/gtk-2.0/gtk.immodules
+"${ETC}"/gtk-2.0/gtk.immodules
 if [[ "$ExistPangoQuerymodules" = "true" ]]; then
     "${GTK_PREFIX}"/bin/pango-querymodules       "${LIB}"/pango/*/modules/*.so          > "${ETC}"/pango/pango.modules
     sed -i "" -e "s|${PWD}|/tmp|" "${ETC}"/pango/pango.modules
@@ -129,23 +129,23 @@ install -d "${MACOS}"/share/themes/Raleigh/gtk-2.0
 (cd "${MACOS}"/share/themes/Raleigh/gtk-2.0 && ln -s ../../Clearlooks/gtk-2.0/gtkrc)
 # fontconfig files (X11 backend only)
 if otool -L "${EXECUTABLE}" | grep -sq 'libgtk-x11-2.0'; then
-  message "Installing fontconfig files (Your library is X11 backend. 'FONTCONFIG_PATH' will be set by executable loader.)"
-  cp -RL "${GTK_PREFIX}"/etc/fonts "${ETC}"
+    message "Installing fontconfig files (Your library is X11 backend. 'FONTCONFIG_PATH' will be set by executable loader.)"
+    cp -RL "${GTK_PREFIX}"/etc/fonts "${ETC}"
 fi
 
 
 
 # install names
 find -E "${MACOS}" -type f -regex '.*/(rawtherapee|.*\.(dylib|so))' | while read x; do
-  message "Modifying install names: ${x}"
-  {
-    # id
+    message "Modifying install names: ${x}"
+    {
+        # id
     case ${x} in *.dylib) echo "   install_name_tool -id '@rpath/$(basename "${x}")' '${x}'";; esac
     # names
     GetDependencies "${x}" | while read y; do
-      echo "   install_name_tool -change '${y}' '@rpath/$(basename "${y}")' '${x}'"
+        echo "   install_name_tool -change '${y}' '@rpath/$(basename "${y}")' '${x}'"
     done
-  } | bash -v
+} | bash -v
 done
 
 message "Registering @loader_path into the executable"
@@ -164,39 +164,39 @@ cp "${PROJECT_SOURCE_DATA_DIR}"/{rawtherapee,profile}.icns "${RESOURCES}"
 cp "${PROJECT_SOURCE_DATA_DIR}"/PkgInfo "${CONTENTS}"
 install -m 0644 "${PROJECT_SOURCE_DATA_DIR}"/Info.plist.in "${CONTENTS}"/Info.plist
 sed -i "" -e "s|@version@|${PROJECT_VERSION}|
-              s|@shortVersion@|${PROJECT_VERSION}|
-              s|@arch@|${arch}|" \
-              "${CONTENTS}"/Info.plist
+s|@shortVersion@|${PROJECT_VERSION}|
+s|@arch@|${arch}|" \
+"${CONTENTS}"/Info.plist
 plutil -convert binary1 "${CONTENTS}"/Info.plist
 
 
 
 function CreateDmg {
-  local srcdir="$(mktemp -dt $$)"
-  
-  message "Preparing disk image sources at ${srcdir}"
-  mv "${APP}" "${srcdir}"
-  cp AboutThisBuild.txt "${srcdir}"
-  ln -s /Applications "${srcdir}"
-  
-  # web bookmarks
-  function CreateWebloc {
-    defaults write "${srcdir}/$1" URL "$2"
-    mv "${srcdir}/$1".{plist,webloc}
-  }
-  CreateWebloc 'RawTherapee Blog' 'http://www.rawtherapee.com'
-  CreateWebloc 'Online Manual'    'http://rawpedia.rawtherapee.com/'
-  
-  # disk image name
-  dmg_name="${PROJECT_NAME// /_}_OSX_${MINIMUM_SYSTEM_VERSION}_${PROC_BIT_DEPTH}_${PROJECT_FULL_VERSION}"
-  if ! echo "${CMAKE_BUILD_TYPE}" | grep -sqi "release"; then
-    dmg_name="${dmg_name}_$(echo ${CMAKE_BUILD_TYPE} | tr '[:upper:]' '[:lower:]')"
-  fi
-  
-  message "Creating disk image"
-  hdiutil create -format UDBZ -srcdir "${srcdir}" -volname "${PROJECT_NAME}_${PROJECT_FULL_VERSION}" "${dmg_name}".dmg
-  
-  message "Removing disk image caches"
-  rm -rf "${srcdir}"
+    local srcdir="$(mktemp -dt $$)"
+
+    message "Preparing disk image sources at ${srcdir}"
+    mv "${APP}" "${srcdir}"
+    cp AboutThisBuild.txt "${srcdir}"
+    ln -s /Applications "${srcdir}"
+
+    # web bookmarks
+    function CreateWebloc {
+        defaults write "${srcdir}/$1" URL "$2"
+        mv "${srcdir}/$1".{plist,webloc}
+    }
+    CreateWebloc 'RawTherapee Blog' 'http://www.rawtherapee.com'
+    CreateWebloc 'Online Manual'    'http://rawpedia.rawtherapee.com/'
+
+    # disk image name
+    dmg_name="${PROJECT_NAME// /_}_OSX_${MINIMUM_SYSTEM_VERSION}_${PROC_BIT_DEPTH}_${PROJECT_FULL_VERSION}"
+    if ! echo "${CMAKE_BUILD_TYPE}" | grep -sqi "release"; then
+        dmg_name="${dmg_name}_$(echo ${CMAKE_BUILD_TYPE} | tr '[:upper:]' '[:lower:]')"
+    fi
+
+    message "Creating disk image"
+    hdiutil create -format UDBZ -srcdir "${srcdir}" -volname "${PROJECT_NAME}_${PROJECT_FULL_VERSION}" "${dmg_name}".dmg
+
+    message "Removing disk image caches"
+    rm -rf "${srcdir}"
 }
 CreateDmg
