80f86c7189c4b88ee51fddbef5874e312aedf648
heckflosse <heckflosse67@gmx.de>
2016-05-14 19:29:26 +0200

    Histogram of working space does not show 'gray2C' for gray regions, fixes #3283, fixes #3213, kudos to mmmtok for providing the patch

--- rtengine/iplab2rgb.cc
+++ rtengine/iplab2rgb.cc
@@ -202,7 +202,7 @@ Image8* ImProcFunctions::lab2rgb (LabImage* lab, int cx, int cy, int cw, int ch,
         }
     } else {
 
-        const auto rgb_xyz = iccStore->workingSpaceMatrix (profile);
+        const auto xyz_rgb = iccStore->workingSpaceInverseMatrix (profile);
 
 #ifdef _OPENMP
         #pragma omp parallel for schedule(dynamic,16) if (multiThread)
@@ -227,7 +227,7 @@ Image8* ImProcFunctions::lab2rgb (LabImage* lab, int cx, int cy, int cw, int ch,
                 float z_ = 65535.0 * Color::f2xyz(fz) * Color::D50z;
                 float y_ = (LL > Color::epskap) ? 65535.0 * fy * fy * fy : 65535.0 * LL / Color::kappa;
 
-                Color::xyz2rgb(x_, y_, z_, R, G, B, rgb_xyz);
+                Color::xyz2rgb(x_, y_, z_, R, G, B, xyz_rgb);
 
                 image->data[ix++] = (int)Color::gamma2curve[CLIP(R)] >> 8;
                 image->data[ix++] = (int)Color::gamma2curve[CLIP(G)] >> 8;
