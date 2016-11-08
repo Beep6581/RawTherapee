59e999fbf548ad4ca3695b25804603ba22a070f2
heckflosse <heckflosse67@gmx.de>
2016-11-08 15:31:48 +0100

    Used stoi instead of atoi

--- rtengine/imagedata.cc
+++ rtengine/imagedata.cc
@@ -446,7 +446,7 @@ void ImageData::extractInfo ()
                         if (baseIsoTag) {
                             std::string isoData = baseIsoTag->valueToString();
                             if (isoData.size() > 1) {
-                                iso_speed = atoi(isoData.c_str());
+                                iso_speed = stoi(isoData);
                             }
                         }
                     }
