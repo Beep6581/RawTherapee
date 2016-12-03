976fc087bd52e8759e81bb7962fc66ea86b73ae9
Hombre <natureh.510@gmail.com>
2016-12-03 11:24:11 +0100

    Removed some debug message left in code

--- rtengine/iplab2rgb.cc
+++ rtengine/iplab2rgb.cc
@@ -169,9 +169,7 @@ Image8* ImProcFunctions::lab2rgb (LabImage* lab, int cx, int cy, int cw, int ch,
         cmsUInt32Number flags = cmsFLAGS_NOOPTIMIZE | cmsFLAGS_NOCACHE;
         if (icm.outputBPC) {
             flags |= cmsFLAGS_BLACKPOINTCOMPENSATION;
-            printf("lab2rgb / bpc=true\n");
         }
-        else printf("lab2rgb / bpc=false\n");
         lcmsMutex->lock ();
         cmsHPROFILE LabIProf  = cmsCreateLab4Profile(nullptr);
         cmsHTRANSFORM hTransform = cmsCreateTransform (LabIProf, TYPE_Lab_DBL, oprofG, TYPE_RGB_8, icm.outputIntent, flags);  // NOCACHE is important for thread safety
@@ -290,19 +288,15 @@ Image16* ImProcFunctions::lab2rgb16 (LabImage* lab, int cx, int cy, int cw, int 
         iccStore->getGammaArray(icm, *ga);
         oprof = iccStore->createGammaProfile(icm, *ga);
         lcmsMutex->unlock ();
-        printf("iccStore->createGammaProfile(icm, *ga);\n");
     } else {
         oprof = iccStore->getProfile (icm.output);
-//        printf("iccStore->getProfile (%s);\n", icm.output.c_str());
     }
 
     if (oprof) {
         cmsUInt32Number flags = cmsFLAGS_NOOPTIMIZE | cmsFLAGS_NOCACHE;
         if (icm.outputBPC) {
             flags |= cmsFLAGS_BLACKPOINTCOMPENSATION;
-//            printf("lab2rgb16 / icm.outputBPC=true / outputIntent=%d\n", icm.outputIntent);
         }
-//        else printf("lab2rgb16 / icm.outputBPC=false / outputIntent=%d\n", icm.outputIntent);
         lcmsMutex->lock ();
         cmsHPROFILE iprof = cmsCreateLab4Profile(nullptr);
         cmsHTRANSFORM hTransform = cmsCreateTransform (iprof, TYPE_Lab_FLT, oprof, TYPE_RGB_16, icm.outputIntent, flags);
