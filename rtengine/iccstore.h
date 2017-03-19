5632c787a3b44f4987dfff33c52d85670d410613
Flössie <floessie.mail@gmail.com>
2017-03-19 11:18:13 +0100

    Don't return `XYZ` for `sRGB` (#3691)
    Also fix length of `ProfileContent` to match the old implementation.

--- rtengine/iccstore.h
+++ rtengine/iccstore.h
@@ -1,12 +1,12 @@
 /*
  *  This file is part of RawTherapee.
  *
- *  Copyright(c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
  *
  *  RawTherapee is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
- *(at your option) any later version.
+ *  (at your option) any later version.
  *
  *  RawTherapee is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
