9e6888070ddb127c2b9534fbc847f87ca832daf2
johenning <johannes.henning@hpi.de>
2017-08-26 16:02:36 +0200

    Added check to avoid out of bounds access

--- rtgui/batchqueue.cc
+++ rtgui/batchqueue.cc
@@ -746,7 +746,7 @@ Glib::ustring BatchQueue::calcAutoFileNameBase (const Glib::ustring& origFileNam
     if (origFileName[0] == '/') {
         pa.push_back ("/" + da[0]);
 	} else if (origFileName[0] == '\\') {
-		if (origFileName[1] == '\\') {
+		if (origFileName.size() > 1 && origFileName[1] == '\\') {
 			pa.push_back ("\\\\" + da[0]);
 		}
 		else {
