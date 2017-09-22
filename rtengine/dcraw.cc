973656fd82720e029c2fb60be8fb33e7d2ad461a
heckflosse <heckflosse67@gmx.de>
2017-09-22 14:03:40 +0200

    Multithreaded sony_arw2_load_raw()

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -3061,61 +3061,59 @@ void CLASS sony_arw_load_raw()
 void CLASS sony_arw2_load_raw()
 {
     BENCHFUN
-//  uchar *data, *dp;
-//  ushort pix[16];
-//  int row, col, val, max, min, imax, imin, sh, bit, i;
 
-//  data = (uchar *) malloc (raw_width+1);
-//  merror (data, "sony_arw2_load_raw()");
+#if defined( _OPENMP ) && defined( MYFILE_MMAP )
 #pragma omp parallel
+#endif
 {
-    IMFILE *ifpthr = new IMFILE; 
-    ifpthr->fd = ifp->fd; 
-    ifpthr->pos = ifp->pos; 
-    ifpthr->size = ifp->size; 
-    ifpthr->data = ifp->data; 
-    ifpthr->eof = ifp->eof; 
-    ifpthr->plistener = nullptr; 
-    ifpthr->progress_range = ifp->progress_range; 
-    ifpthr->progress_next = ifp->progress_next; 
-    ifpthr->progress_current = ifp->progress_current;
-    uchar *data, *dp;
-    data = (uchar *) malloc (raw_width+1);
-    merror (data, "sony_arw2_load_raw()");
-    ushort pix[16];
-    int val;
-    int col;
-    int sh;
-    int i;
-    int bit;
-    int pos = ifpthr->pos;
-#pragma omp for
-
-  for (int row=0; row < height; row++) {
-    fseek(ifpthr, pos + row * raw_width, SEEK_SET);
-    fread (data, 1, raw_width, ifpthr);
-    for (dp=data, col=0; col < raw_width-30; dp+=16) {
-      int max = 0x7ff & (val = sget4(dp));
-      int min = 0x7ff & val >> 11;
-      int imax = 0x0f & val >> 22;
-      int imin = 0x0f & val >> 26;
-      for (sh=0; sh < 4 && 0x80 << sh <= max-min; sh++);
-      for (bit=30, i=0; i < 16; i++)
-	if      (i == imax) pix[i] = max;
-	else if (i == imin) pix[i] = min;
-	else {
-	  pix[i] = ((sget2(dp+(bit >> 3)) >> (bit & 7) & 0x7f) << sh) + min;
-	  if (pix[i] > 0x7ff) pix[i] = 0x7ff;
-	  bit += 7;
-	}
-      for (i=0; i < 16; i++, col+=2)
-       RAW(row,col) = curve[pix[i] << 1]; // >> 2; RT: disabled shifting to avoid precision loss
-      col -= col & 1 ? 1:31;
+    IMFILE ifpthr = *ifp;
+#if defined( _OPENMP ) && defined( MYFILE_MMAP )
+    #pragma omp master
+#endif
+    {
+    ifpthr.plistener = ifp->plistener;
     }
-  }
-  free (data);
-  delete ifpthr;
+    uchar *data = new (std::nothrow) uchar[raw_width + 1];
+    merror(data, "sony_arw2_load_raw()");
+    ushort pix[16];
+    int pos = ifpthr.pos;
 
+#if defined( _OPENMP ) && defined( MYFILE_MMAP )
+    #pragma omp for schedule(dynamic,16) nowait
+#endif
+
+    for (int row = 0; row < height; row++) {
+        fseek(&ifpthr, pos + row * raw_width, SEEK_SET);
+        fread (data, 1, raw_width, &ifpthr);
+        uchar *dp = data;
+        for (int col = 0; col < raw_width - 30; dp += 16) {
+            int val = sget4(dp);
+            int max = 0x7ff & val;
+            int min = 0x7ff & val >> 11;
+            int imax = 0x0f & val >> 22;
+            int imin = 0x0f & val >> 26;
+            int bit = 30;
+            for (int i=0; i < 16; i++) {
+                if (i == imax) {
+                    pix[i] = max;
+                } else if (i == imin) {
+                    pix[i] = min;
+                } else {
+                    int sh;
+                    for (sh = 0; sh < 4 && 0x80 << sh <= max-min; sh++)
+                        ;
+                    pix[i] = ((sget2(dp + (bit >> 3)) >> (bit & 7) & 0x7f) << sh) + min;
+                    pix[i] = std::min(pix[i], (ushort)0x7ff);
+                    bit += 7;
+                }
+            }
+            for (int i = 0; i < 16; i++, col += 2) {
+                RAW(row,col) = curve[pix[i] << 1]; // >> 2; RT: disabled shifting to avoid precision loss
+            }
+            col -= col & 1 ? 1:31;
+        }
+    }
+  delete [] data;
 }
   maximum = curve[0x7ff << 1]; // RT: fix maximum.
   maximum = 16300; // RT: conservative white level tested on various ARW2 cameras. This constant was set in 2013-12-17, may need re-evaluation in the future.
