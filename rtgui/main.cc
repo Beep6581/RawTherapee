4313cfc74c2a472e3c6b54282ae64bf43ca3d239
Alberto Griggio <agriggio@users.noreply.github.com>
2017-09-25 12:09:40 +0200

    fixed default value of lensfunDbDirectory when BUILD_BUNDLE is set

--- rtgui/main.cc
+++ rtgui/main.cc
@@ -529,7 +529,7 @@ int main (int argc, char **argv)
         licensePath = Glib::build_filename (exePath, LICENCE_SEARCH_PATH);
     }
 
-    if (Glib::path_is_absolute (LENSFUN_DB_PATH)) {
+    if (strlen(LENSFUN_DB_PATH) == 0 || Glib::path_is_absolute (LENSFUN_DB_PATH)) {
         options.rtSettings.lensfunDbDirectory = LENSFUN_DB_PATH;
     } else {
         options.rtSettings.lensfunDbDirectory = Glib::build_filename (exePath, LENSFUN_DB_PATH);
