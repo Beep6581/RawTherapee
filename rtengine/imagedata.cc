80b23e7168a7a66a39a5f3284e3cd5e1eaabaa21
heckflosse <heckflosse67@gmx.de>
2017-10-23 20:59:47 +0200

    Make compilation unit rtengine/imagedata.cc -Wextra clean, #4155

--- rtengine/imagedata.cc
+++ rtengine/imagedata.cc
@@ -24,7 +24,7 @@
 #include "iptcpairs.h"
 #include "imagesource.h"
 #include "rt_math.h"
-
+#pragma GCC diagnostic warning "-Wextra"
 #define PRINT_HDR_PS_DETECTION 0
 
 using namespace rtengine;
@@ -1041,7 +1041,7 @@ failure:
 
 }
 
-FramesData::FramesData (const Glib::ustring& fname, std::unique_ptr<RawMetaDataLocation> rml, bool firstFrameOnly, bool loadAll) :
+FramesData::FramesData (const Glib::ustring& fname, std::unique_ptr<RawMetaDataLocation> rml, bool firstFrameOnly) :
     iptc(nullptr), dcrawFrameCount (0)
 {
     if (rml && (rml->exifBase >= 0 || rml->ciffBase >= 0)) {
