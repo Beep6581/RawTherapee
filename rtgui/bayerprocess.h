8d3bb7d5802ba4e24afb89dd4cf13ff8aafef392
heckflosse <heckflosse67@gmx.de>
2017-12-20 18:16:42 +0100

    Temporary remove pixel shift one green from gui

--- rtgui/bayerprocess.h
+++ rtgui/bayerprocess.h
@@ -49,7 +49,7 @@ protected:
     CheckBox* pixelShiftBlur;
     CheckBox* pixelShiftHoleFill;
     CheckBox* pixelShiftMedian;
-    CheckBox* pixelShiftOneGreen;
+//    CheckBox* pixelShiftOneGreen;
     CheckBox* pixelShiftLmmse;
     CheckBox* pixelShiftEqualBright;
     CheckBox* pixelShiftEqualBrightChannel;
