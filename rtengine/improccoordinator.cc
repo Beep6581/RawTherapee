5b11da17e077c9ea2768391f6a059350a975064f
Desmis <jdesmis@gmail.com>
2018-04-04 08:57:22 +0200

    Change slightly LUT 212

--- rtengine/improccoordinator.cc
+++ rtengine/improccoordinator.cc
@@ -315,21 +315,21 @@ void ImProcCoordinator::updatePreviewImage (int todo, Crop* cropCall)
         if (params.wb.method == "Auto" && awbListener && params.wb.enabled) {
             awbListener->WBChanged (params.wb.temperature, params.wb.green);
         }
-		/*
+		
             Glib::ustring datal;
 			datal = "lutsrgb.txt";
 		            ofstream fou(datal, ios::out | ios::trunc);
 	
-		for(int i=0; i < 1024; i++) {
+		for(int i=0; i < 212; i++) {
 			//printf("igamma2=%i\n", (int) 65535.f*Color::igamma2(i/212.0));
-					float gam = Color::igamma2(i/1023.0);
+					float gam = Color::igamma2(i/211.0);
 					int lutga = 65535.f* gam;
                   //  fou << 65535*(int)Color::igamma2(i/212.0) << endl;
                     fou << i << " " << lutga << endl;
 			
 		}	
                 fou.close();
-*/		
+		
         int tr = getCoarseBitMask (params.coarse);
 
         imgsrc->getFullSize (fw, fh, tr);
