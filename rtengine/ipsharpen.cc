Beep6581/RawTherapee

commit 03efc48bc51e00c0bf34bb3bb7b0311ab1ff925f

tentative fix for #4481

rtengine/ipsharpen.cc
@@ -106,7 +106,7 @@ void ImProcFunctions::deconvsharpening (float** luminance, float** tmp, int W, i
 
     for (int i = 0; i < H; i++) {
         for(int j = 0; j < W; j++) {
-            tmpI[i][j] = luminance[i][j];
+            tmpI[i][j] = max(luminance[i][j], 0.f);
         }
     }
 
@@ -122,6 +122,11 @@ void ImProcFunctions::deconvsharpening (float** luminance, float** tmp, int W, i
             if (!needdamp) {
                 // apply gaussian blur and divide luminance by result of gaussian blur
                 gaussianBlur (tmpI, tmp, W, H, sigma, nullptr, GAUSS_DIV, luminance);
+                for (int i = 0; i < H; i++) {
+                    for(int j = 0; j < W; j++) {
+                        tmp[i][j] = max(tmp[i][j], 0.f);
+                    }
+                }
             } else {
                 // apply gaussian blur + damping
                 gaussianBlur (tmpI, tmp, W, H, sigma);

commit 68ecc24270288510625c0bf1efc362ea6a84372b

Fix slowdown caused by 03efc48

rtengine/ipsharpen.cc
@@ -122,6 +122,9 @@ void ImProcFunctions::deconvsharpening (float** luminance, float** tmp, int W, i
             if (!needdamp) {
                 // apply gaussian blur and divide luminance by result of gaussian blur
                 gaussianBlur (tmpI, tmp, W, H, sigma, nullptr, GAUSS_DIV, luminance);
+#ifdef _OPENMP
+                #pragma omp for
+#endif
                 for (int i = 0; i < H; i++) {
                     for(int j = 0; j < W; j++) {
                         tmp[i][j] = max(tmp[i][j], 0.f);