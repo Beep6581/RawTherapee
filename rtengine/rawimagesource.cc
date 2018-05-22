7d49895769734d7a9885620c36b14803805260eb
heckflosse <heckflosse67@gmx.de>
2018-05-22 14:38:32 +0200

    Removed Stopwatches

--- rtengine/rawimagesource.cc
+++ rtengine/rawimagesource.cc
@@ -39,7 +39,6 @@
 #include <omp.h>
 #endif
 #include "opthelper.h"
-#include "StopWatch.h"
 #define clipretinex( val, minv, maxv )    (( val = (val < minv ? minv : val ) ) > maxv ? maxv : val )
 #undef CLIPD
 #define CLIPD(a) ((a)>0.0f?((a)<1.0f?(a):1.0f):0.0f)
@@ -2015,7 +2014,6 @@ void RawImageSource::preprocess  (const RAWParams &raw, const LensProfParams &le
             plistener->setProgress (0.0);
         }
         if(numFrames == 4) {
-            StopWatch Stop1("ps ca correction");
             double fitParams[64];
             float *buffer = CA_correct_RT(raw.ca_autocorrect, raw.cared, raw.cablue, 8.0, *rawDataFrames[0], fitParams, false, true, nullptr, false);
             for(int i = 1; i < 3; ++i) {
