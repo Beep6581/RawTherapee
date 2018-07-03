ccfe4275d17d445792acbf77cd9a1400ad9c6bce
heckflosse <heckflosse67@gmx.de>
2018-07-03 21:07:39 +0200

    use __m128i instead of __m128i_u

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -10018,13 +10018,13 @@ static void expandFloats(Bytef * dst, int tileWidth, int bytesps) {
     float* const dst32 = reinterpret_cast<float*>(dst);
     int index = tileWidth - 8;
     for (; index >= 0; index -= 8) {
-        __m128i halfFloatv = _mm_loadu_si128((__m128i_u*)&dst16[index]);
+        __m128i halfFloatv = _mm_loadu_si128((__m128i*)&dst16[index]);
         STVFU(dst32[index], _mm_cvtph_ps(halfFloatv));
         STVFU(dst32[index + 4], _mm_cvtph_ps(_mm_shuffle_epi32(halfFloatv, _MM_SHUFFLE(0,0,3,2))));
     }
     index += 4;
     if(index >= 0) {
-        __m128i halfFloatv = _mm_loadu_si128((__m128i_u*)&dst16[index]);
+        __m128i halfFloatv = _mm_loadu_si128((__m128i*)&dst16[index]);
         STVFU(dst32[index], _mm_cvtph_ps(halfFloatv));
         index--;
     } else {
