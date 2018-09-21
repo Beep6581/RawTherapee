bb5e6ef234dd08de9f86f7ef64a3d29e83a566f9
heckflosse <heckflosse67@gmx.de>
2018-09-21 17:32:41 +0200

    Define raw crop for Sony ILCE-7M2 arw and dng files in camconst.json, fixes #4585

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -9874,7 +9874,8 @@ konica_400z:
     width -= height > 3664 ? 8 : 32;
     if (!strncmp(model,"DSC",3))
       black = 200 << (tiff_bps - 12);
-  } else if (!strcmp(make,"Sony") && raw_width == 6048) {
+  } else if (!strcmp(make,"Sony") && strcmp(model,"ILCE-7M2") && raw_width == 6048) {
+    // for Sony ILCE-7M2 the raw crop is defined in camconst.json
     width -= 24;
     if (strstr(model,"RX1") || strstr(model,"A99"))
       width -= 6;
