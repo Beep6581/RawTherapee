8e3e4afcaf9f1dd4a9ace5deeaaef90d8f1efbb6
Morgan Hardwood <bugs@londonlight.org>
2018-12-31 16:40:31 +0100

    Select+focus thumbnail in Filmstrip, closes #5125
    When passing a photo as an argument to the RawTherapee executable, the user wants
    this auto-loaded photo's thumbnail to be selected and visible in the Filmstrip.

--- rtgui/editorpanel.cc
+++ rtgui/editorpanel.cc
@@ -1083,6 +1083,10 @@ void EditorPanel::open (Thumbnail* tmb, rtengine::InitialImage* isrc)
 
     history->resetSnapShotNumber();
     navigator->setInvalid(ipc->getFullWidth(),ipc->getFullHeight());
+
+    // When passing a photo as an argument to the RawTherapee executable, the user wants
+    // this auto-loaded photo's thumbnail to be selected and visible in the Filmstrip.
+    EditorPanel::syncFileBrowser();
 }
 
 void EditorPanel::close ()
