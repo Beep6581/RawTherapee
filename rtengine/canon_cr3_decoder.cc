98da38aaebd10b445ac6e4b41e357f4092a889a8
Flössie <floessie.mail@gmail.com>
2020-05-17 09:55:41 +0200

    Fix leak in `crxFreeImageData()` (by Danny Heijl), fixes #5758

--- rtengine/canon_cr3_decoder.cc
+++ rtengine/canon_cr3_decoder.cc
@@ -3036,11 +3036,11 @@ bool crxSetupImageData(
 
 void crxFreeImageData(CrxImage* img)
 {
-    CrxTile* tile = img->tiles;
-    const int nTiles = img->tileRows * img->tileCols;
-
     if (img->tiles) {
-        for (std::int32_t curTile = 0; curTile < nTiles; curTile++, tile++) {
+        CrxTile* const tile = img->tiles;
+        const int nTiles = img->tileRows * img->tileCols;
+
+        for (std::int32_t curTile = 0; curTile < nTiles; ++curTile) {
             if (tile[curTile].comps) {
                 for (std::int32_t curPlane = 0; curPlane < img->nPlanes; ++curPlane) {
                     crxFreeSubbandData(img, tile[curTile].comps + curPlane);
