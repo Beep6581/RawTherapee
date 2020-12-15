004792d3fd1a75a108e4025514102e0edd835180
Flössie <floessie.mail@gmail.com>
2020-12-15 15:24:04 +0100

    Fix ResizeParams defaults

--- rtengine/procparams.cc
+++ rtengine/procparams.cc
@@ -2172,7 +2172,7 @@ bool CACorrParams::operator !=(const CACorrParams& other) const
 
 ResizeParams::ResizeParams() :
     enabled(false),
-    scale(1.0),
+    scale(0.17000000000000001),
     appliesTo("Cropped area"),
     method("Lanczos"),
     dataspec(3),
