Benitoite/ART

metadata: initialize thumbnail rating from exif

rtengine/imagedata.cc
@@ -63,7 +63,8 @@ FramesData::FramesData(const Glib::ustring &fname):
     lens("Unknown"),
     sampleFormat(IIOSF_UNKNOWN),
     isPixelShift(false),
-    isHDR(false)
+    isHDR(false),
+    rating_(0)
 {
     memset(&time, 0, sizeof(time));
     timeStamp = 0;
@@ -242,6 +243,10 @@ FramesData::FramesData(const Glib::ustring &fname):
 
         if (find_exif_tag("Exif.Image.ExposureBiasValue")) {
             expcomp = pos->toFloat();
+        }
+
+        if (find_exif_tag("Exif.Image.Rating")) {
+            rating_ = pos->toLong();
         }
 
         // -----------------------
@@ -544,6 +549,12 @@ unsigned int FramesData::getFrameCount() const
 Glib::ustring FramesData::getFileName() const
 {
     return fname_;
+}
+
+
+int FramesData::getRating() const
+{
+    return rating_;
 }
 
 //------inherited functions--------------//

Beep6581/RawTherapee

(cherry picked from commit 96bd2db0d2e127678bded3bee7aa3275332826c0)

rtengine/imagedata.cc
@@ -287,6 +287,10 @@ FramesData::FramesData(const Glib::ustring &fname) :
             expcomp = pos->toFloat();
         }
 
+        if (find_exif_tag("Exif.Image.Rating")) {
+            rating = pos->toLong();
+        }
+
         // -----------------------
         // Special file type detection (HDR, PixelShift)
         // ------------------------
@@ -567,12 +571,6 @@ std::string FramesData::getOrientation() const
 }
 
 
-int FramesData::getRating() const
-{
-    return rating;
-}
-
-
 void FramesData::setDCRawFrameCount(unsigned int frameCount)
 {
     dcrawFrameCount = frameCount;
@@ -587,6 +585,12 @@ unsigned int FramesData::getFrameCount() const
 Glib::ustring FramesData::getFileName() const
 {
     return fname_;
+}
+
+
+int FramesData::getRating() const
+{
+    return rating;
 }
 
 //------inherited functions--------------//