rnwgnr/ART

file browser: pick up star ratings from the image metadata if available

rtengine/imagedata.cc
@@ -285,6 +285,11 @@ FramesData::FramesData(const Glib::ustring &fname):
 
         if (find_exif_tag("Exif.Image.Rating")) {
             rating_ = pos->toLong();
+        } else {
+            auto it = meta.xmpData().findKey(Exiv2::XmpKey("Xmp.xmp.Rating"));
+            if (it != meta.xmpData().end() && it->size()) {
+                rating_ = it->toLong();
+            }
         }
 
         // -----------------------

Beep6581/RawTherapee

(cherry picked from commit 98f0675b2e419d18bd88259997b81bcbcc4e5c96)

rtengine/imagedata.cc
@@ -325,6 +325,11 @@ FramesData::FramesData(const Glib::ustring &fname) :
 
         if (find_exif_tag("Exif.Image.Rating")) {
             rating = pos->toLong();
+        } else {
+            auto it = meta.xmpData().findKey(Exiv2::XmpKey("Xmp.xmp.Rating"));
+            if (it != meta.xmpData().end() && it->size()) {
+                rating = it->toLong();
+            }
         }
 
         // -----------------------