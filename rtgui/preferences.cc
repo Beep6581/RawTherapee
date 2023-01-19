9f0ebc274af550e1bf7ba6dc1ba7a67c09d20a4a
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2023-01-19 15:16:44 +0100

    Change tooltip Itcwb preferences

--- rtgui/preferences.cc
+++ rtgui/preferences.cc
@@ -936,7 +936,6 @@ Gtk::Widget* Preferences::getColorManPanel ()
 	setExpandAlignProperties(mwbacorr, false, false, Gtk::ALIGN_START, Gtk::ALIGN_CENTER);
     mwbacorr->set_active(true);
     wbah->pack_start(*mwbacorr, Gtk::PACK_SHRINK, 0);
-//   wbaVB->add(*wbah);
 
     mwbasort = Gtk::manage(new Gtk::CheckButton(M("PREFERENCES_WBASORT")));
 	setExpandAlignProperties(mwbasort, false, false, Gtk::ALIGN_START, Gtk::ALIGN_CENTER);
@@ -954,7 +953,6 @@ Gtk::Widget* Preferences::getColorManPanel ()
     wbah->pack_start(*mwbanopurp, Gtk::PACK_SHRINK, 0);
 	
     wbaVB->add(*wbah);
-	//int digits, int inc0, int inc1, int maxLength, int range0, int range1,
     placeSpinBox(wbaVB, wbacorrnb, "PREFERENCES_WBAPATCH", 0, 1, 5, 2, 10, 55);
     placeSpinBox(wbaVB, wbaprecis, "PREFERENCES_WBAPRECIS", 0, 2, 5, 2, 3, 7);
     placeSpinBox(wbaVB, wbasizeref, "PREFERENCES_WBASIZEREF", 0, 1, 5, 2, 1, 5);
