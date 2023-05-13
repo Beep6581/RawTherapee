aee80fb0ce32df7c631533dca847db756264e4d3
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2023-05-13 09:17:56 +0200

    marks out the code with past time markers

--- rtengine/rawimagesource.cc
+++ rtengine/rawimagesource.cc
@@ -5233,6 +5233,9 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
     Itcwb_minsize: minimal size of the patch
     */
     BENCHFUN
+    MyTime t1, t2, t3, t4, t5, t6, t7, t8;
+    t1.set();
+
     bool itciterate = true;
     bool lastitc = true;
 
@@ -5724,6 +5727,11 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
             bmm[tt] = bm / gm;
             //return rmm, gmm, bmm in function of temp
         }
+    t2.set();
+
+    if (settings->verbose) {
+        printf("First: up calculate multipliers: %d nsec\n",  t2.etime(t1));
+    }
 
         struct hiss {//histogram
             int histnum;
@@ -5852,6 +5860,11 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
             }
 
             delete[] tmL;
+        }
+        t3.set();
+
+        if (settings->verbose) {
+            printf("Second: from first to up median 3x3: %d nsec\n", t3.etime(t2));
         }
 
 #ifdef _OPENMP
@@ -6293,6 +6306,12 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
             G_curref_reduc[i][repref] = g / gmm[repref];
             B_curref_reduc[i][repref] = b / bmm[repref];
         }
+        t4.set();
+
+        if (settings->verbose) {
+            printf("Third: from second to find patch: %d msec\n", t4.etime(t3));
+        }
+
 
 //end first part
 
@@ -6305,6 +6324,11 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
         array2D<float> xxyycurr_reduc(N_t, 2 * sizcurr);
         array2D<float> reff_spect_xxyy(N_t, 2 * Nc + 2);
         array2D<float> reff_spect_xxyy_prov(N_t, 2 * Nc + 2);
+        t5.set();
+
+        if (settings->verbose) {
+            printf("Fourth: from third recalculate spectral: %d msec\n",  t5.etime(t4));
+        }
 
         float minstud = 100000.f;
         int goodref = 1;
@@ -6316,10 +6340,10 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
         if(wbpar.itcwb_custom) {
             ttbeg = std::max(repref - 5, 0);
             ttend = std::min(repref + 5, N_t);
-        } /* else {
+        }  else {
             ttbeg = std::max(repref - 10, 0);
             ttend = std::min(repref + 10, N_t);
-        } */
+        } 
 
         for (int tt = ttbeg; tt < ttend; ++tt) {//N_t
             for (int i = 0; i < w; ++i) {
@@ -6353,6 +6377,11 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
                 minstud = abstud;
                 goodref = tt;
             }
+        }
+        t6.set();
+
+        if (settings->verbose) {
+            printf("Fifth: from fourth to find first correlation: %d msec\n",  t6.etime(t5));
         }
 
         {//always used if extra = true because I made this choice, brings better results
@@ -6455,6 +6484,11 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
                     Tgstud[gr].student = minstudgr;
 
                 }
+            }
+            t7.set();
+
+            if (settings->verbose) {
+                printf("Sixth: from fifth to find patch in extra mode: %d nsec\n", t7.etime(t6));
             }
 
             float estimchromf = 0.f;
@@ -6706,6 +6740,12 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
         avg_gm = optitc[0].avg_g;
         avg_bm = optitc[0].avg_b;
     }
+        t8.set();
+
+        if (settings->verbose) {
+            printf("Seventh: from sixth to end: %d msec\n",  t8.etime(t7));
+        }
+    
 }
 
 void RawImageSource::WBauto(bool extra, double & tempref, double & greenref, array2D<float> &redloc, array2D<float> &greenloc, array2D<float> &blueloc, int bfw, int bfh, double & avg_rm, double & avg_gm, double & avg_bm, double & tempitc, double & greenitc, float &temp0, float &delta, int &bia,  int &dread, int &kcam, float & studgood, float &minchrom, int &kmin, float &minhist, float &maxhist, bool & twotimes, const WBParams & wbpar, int begx, int begy, int yEn, int xEn, int cx, int cy, const ColorManagementParams & cmp, const RAWParams & raw, const ToneCurveParams &hrp)
