c97a069c23322383581e92828bb8ea583b1c0032
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2023-06-05 10:20:57 +0200

    Allows dispaly in console xy values thanks to Reffort

--- rtengine/rawimagesource.cc
+++ rtengine/rawimagesource.cc
@@ -4546,7 +4546,9 @@ static void histoxyY_low(int bfhitc, int bfwitc, const array2D<float> & xc, cons
 }
 
 
+//enable display cells
 
+//int cellxy[80][90] ;
 
 static void histoxyY(int bfhitc, int bfwitc, const array2D<float> & xc, const array2D<float> & yc, const array2D<float> & Yc, LUTf &xxx, LUTf &yyy, LUTf &YYY, LUTu &histxy, bool purpe)
 {
@@ -4554,8 +4556,11 @@ static void histoxyY(int bfhitc, int bfwitc, const array2D<float> & xc, const ar
     // this "choice" are guided by generally colors who are in nature skin, sky, etc. in those cases "steps" are small
     // of course we can change to be more precise
     // purp enable or not purple color in xyY - approximation...
+//enable display cells
+//    int totalpixels = 0;
+
 #ifdef _OPENMP
-    #pragma omp parallel
+    #pragma omp parallel  // disabled if enable display cells
 #endif
     {
         LUTu histxythr(histxy.getSize());
@@ -4569,9 +4574,18 @@ static void histoxyY(int bfhitc, int bfwitc, const array2D<float> & xc, const ar
         bool purp = true;
         float Ypurp = settings->itcwb_Ypurple;
         float Ypurpmax = 1.f;
-
+        //enable display cells
+/*
+        // clear
+        for (int i = 0; i < 80; ++i) {
+            for (int j = 0 ; j < 90; ++j) {
+                cellxy[i][j] = 0;
+            }
+        }
+*/
 #ifdef _OPENMP
-        #pragma omp for schedule(dynamic, 4) nowait
+        #pragma omp for schedule(dynamic, 4) nowait //disable if enable display cells
+
 #endif
 
         for (int y = 0; y < bfhitc ; y++)
@@ -5103,11 +5117,22 @@ static void histoxyY(int bfhitc, int bfwitc, const array2D<float> & xc, const ar
                     yyythr[nh] += yc[y][x];
                     YYYthr[nh] += Yc[y][x];
                 }
+//enable display cells
+/*                    
+                    // update
+                    int x1 = (int)(100.0 * (xc[y][x]));
+                    int y1 = (int)(100.0 * (yc[y][x]));
+        
+                    if (x1 >= 0 && x1 < 80 && y1 >= 0 && y1 < 90) {
+                        cellxy[x1][y1]++;
+                        totalpixels++;
+                    }
+*/
             }
         }
 
 #ifdef _OPENMP
-        #pragma omp critical
+        #pragma omp critical    //disable if enable display cells
 #endif
         {
             histxy += histxythr;
@@ -6131,6 +6156,18 @@ void RawImageSource::ItcWB(bool extra, double &tempref, double &greenref, double
         } else {
             histoxyY_low(bfhitc, bfwitc, xc, yc, Yc, xxx,  yyy, YYY, histxy);//low scaling
         }
+//enable display cells
+/*        
+        printf ("xc\tyc\tcount\n") ;
+        printf ("--\t--\t-----\n") ;
+        for (int x1 = 0 ; x1 < 80; ++x1) {
+            for (int y1 = 0; y1 < 90; ++y1) {
+                if (cellxy[x1][y1] > 0) {
+                    printf ("%d\t%d\t%d\n", x1, y1, cellxy[x1][y1]);
+                }
+            }
+        }
+*/
 
         // free some memory
         xc.free();
