e6a2654241978c17f1f0277fa375382fbfe6e420
Lawrence Lee <45837045+Lawrence37@users.noreply.github.com>
2023-08-27 16:32:30 -0700

    Show "unchanged" in batch profiled lens correction

--- rtgui/lensprofile.cc
+++ rtgui/lensprofile.cc
@@ -441,6 +441,7 @@ void LensProfilePanel::setBatchMode(bool yes)
     modesGrid->attach_next_to(*corrUnchangedRB, Gtk::POS_TOP, 3, 1);
     corrUnchangedRB->signal_toggled().connect(sigc::bind(sigc::mem_fun(*this, &LensProfilePanel::onCorrModeChanged), corrUnchangedRB));
     corrUnchangedRB->set_active(true);
+    corrUnchangedRB->show();
 }
 
 void LensProfilePanel::onLensfunCameraChanged()
