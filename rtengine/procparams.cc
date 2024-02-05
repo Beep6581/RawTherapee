c9ccd7b6021507c4437891bdea9a9a47952c1478
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2024-02-05 17:25:57 +0100

    LA settings avoidcolorshift XYZabsolute

--- rtengine/procparams.cc
+++ rtengine/procparams.cc
@@ -2982,7 +2982,7 @@ LocallabParams::LocallabSpot::LocallabSpot() :
     structexclu(0),
     struc(4.0),
     shapeMethod("IND"),
-    avoidgamutMethod("MUNS"),
+    avoidgamutMethod("XYZ"),
     loc{150, 150, 150, 150},
     centerX(0),
     centerY(0),
