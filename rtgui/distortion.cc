53a64a923582956639cd9db50dc4a9d3b431df81
Alexander Brock <alexander@lunar-orbit.de>
2024-09-12 21:06:52 +0200

    Make history event correctly show "Defish - Focal Length" when changing the focal length of the defish tool

--- rtgui/distortion.cc
+++ rtgui/distortion.cc
@@ -128,7 +128,12 @@ void Distortion::setDefaults (const ProcParams* defParams, const ParamsEdited* p
 void Distortion::adjusterChanged(Adjuster* a, double newval)
 {
     if (listener) {
-        listener->panelChanged (EvDISTAmount, Glib::ustring::format (std::setw(4), std::fixed, std::setprecision(3), a->getValue()));
+        if (a == focal_length) {
+            listener->panelChanged (*event_distortion_defish_focal_length, Glib::ustring::format (std::setw(4), std::fixed, std::setprecision(2), a->getValue()));
+        }
+        else if (a == distor) {
+            listener->panelChanged (EvDISTAmount, Glib::ustring::format (std::setw(4), std::fixed, std::setprecision(3), a->getValue()));
+        }
     }
 }
 
