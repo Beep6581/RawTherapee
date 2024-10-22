59054e1a0e81b2429ec1ee3b32f7a482f6c1b55c
Lawrence Lee <45837045+Lawrence37@users.noreply.github.com>
2024-10-21 21:04:17 -0700

    Update copyright for gamut compression GUI

--- rtgui/compressgamut.cc
+++ rtgui/compressgamut.cc
@@ -1,7 +1,7 @@
 /*
  *  This file is part of RawTherapee.
  *
- *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *  Copyright (c) 2024 RawTherapee team
  *
  *  RawTherapee is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
