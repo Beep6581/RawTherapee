701c68d8a2bbce7eecaa91324928dcc52f6fac38
jacques <jdesmis@gmail.com>
2025-02-21 07:07:01 +0100

    Remove warnings in icmpanel

--- rtgui/icmpanel.cc
+++ rtgui/icmpanel.cc
@@ -1467,8 +1467,7 @@ void ICMPanel::write(ProcParams* pp, ParamsEdited* pedited)
 
     pp->icm.workingProfile = wProfNames->get_active_text();
     pp->icm.dcpIlluminant = rtengine::max<int>(dcpIll->get_active_row_number(), 0);
-    double zerox = 0.;
-    double zeroy = 0.;
+
 
     labgridcie->getParams(pp->icm.labgridcieALow, pp->icm.labgridcieBLow, pp->icm.labgridcieAHigh, pp->icm.labgridcieBHigh, pp->icm.labgridcieGx, pp->icm.labgridcieGy, pp->icm.labgridcieWx, pp->icm.labgridcieWy, pp->icm.labgridcieMx, pp->icm.labgridcieMy);
 
@@ -1599,8 +1598,7 @@ void ICMPanel::setDefaults(const ProcParams* defParams, const ParamsEdited* pedi
     shiftx->setDefault(defParams->icm.shiftx);
     shifty->setDefault(defParams->icm.shifty);
     preser->setDefault(defParams->icm.preser);
-    double zerox = 0.;
-    double zeroy = 0.;
+
 
     labgridcie->setDefault(defParams->icm.labgridcieALow, defParams->icm.labgridcieBLow , defParams->icm.labgridcieAHigh, defParams->icm.labgridcieBHigh, defParams->icm.labgridcieGx, defParams->icm.labgridcieGy, defParams->icm.labgridcieWx, defParams->icm.labgridcieWy, defParams->icm.labgridcieMx, defParams->icm.labgridcieMy);
 
