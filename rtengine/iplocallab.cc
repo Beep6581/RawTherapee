b2c989d2afa4f190bcd068957539a6b98a5d7605
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2025-02-26 17:07:14 +0100

    Limits negatives values control to cbdl before

--- rtengine/iplocallab.cc
+++ rtengine/iplocallab.cc
@@ -14297,7 +14297,12 @@ void ImProcFunctions::Lab_Local(
 
     float epsi0 = 0.000001f;
     bool nocrash = false;
-    bool dirpyrdeha = params->dirpyrequalizer.enabled  ||  params->dehaze.enabled  || lp.avoidneg;//lp.avoidneg in setting 
+    bool cbdl = false;
+    if(params->dirpyrequalizer.cbdlMethod == "bef") {//If user choose "after black and white" this function which removes negative values is not used, hence CBDL is best performed, after Selective Editing in Lab mode
+        cbdl = true;
+    }
+    bool dirpyrdeha = (params->dirpyrequalizer.enabled && cbdl)  ||  params->dehaze.enabled  || lp.avoidneg;//lp.avoidneg in setting 
+    
     if(dirpyrdeha) {
         nocrash = true;
     }
