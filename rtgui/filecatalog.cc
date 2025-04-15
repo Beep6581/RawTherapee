8782530a490ba441f404987a75d5320f27aa9b9f
kaesa <kaesa@users.noreply.github.com>
2025-04-15 17:35:42 +0300

    Fix for files not reappearing when moved out and then back from observed directory.

--- rtgui/filecatalog.cc
+++ rtgui/filecatalog.cc
@@ -1810,7 +1810,11 @@ void FileCatalog::reparseDirectory (bool onlyDelete)
 
     // when handling FILE_MONITOR_EVENT_DELETED we can stop here, otherwise there can be significant performance penalty
     // with network shares
-    if (onlyDelete) {
+    if (onlyDelete && fileNamesToRemove.size() == 1) {
+        auto pos = std::find(fileNameList.cbegin(), fileNameList.cend(), *fileNamesToRemove.cbegin());
+        if (pos != fileNameList.cend()) {
+            fileNameList.erase(pos);
+        }
         return;
     }
 
