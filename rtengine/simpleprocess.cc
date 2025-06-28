2ebed6f97660e714782a8fbdb8126f1df9c14fd0
U-PCSPECIALIST01\jdesm <jdesmis@gmail.com>
2025-06-28 14:38:12 +0200

    test to see if canceling workingtrc in simpleprocess is effective

--- rtengine/simpleprocess.cc
+++ rtengine/simpleprocess.cc
@@ -1588,7 +1588,7 @@ private:
         }
 
         int savestr = params.wavelet.strength;//work around for abstract profile: time about = 0.1 second
-        if ((params.wavelet.enabled)  || (params.icm.workingTRC != ColorManagementParams::WorkingTrc::NONE  && params.icm.trcExp)) {
+        if ((params.wavelet.enabled)){//  || (params.icm.workingTRC != ColorManagementParams::WorkingTrc::NONE  && params.icm.trcExp)) {
             LabImage *unshar = nullptr;
             WaveletParams WaveParams = params.wavelet;
             WavCurve wavCLVCurve;
@@ -1608,9 +1608,9 @@ private:
             bool proton = WaveParams.exptoning;
             bool pronois = WaveParams.expnoise;
 
-            if(params.icm.workingTRC != ColorManagementParams::WorkingTrc::NONE  && params.icm.trcExp) {
-                params.wavelet.strength = 0;
-            }
+          //  if(params.icm.workingTRC != ColorManagementParams::WorkingTrc::NONE  && params.icm.trcExp) {
+          //      params.wavelet.strength = 0;
+          //  }
             if (WaveParams.softrad > 0.f) {
                 provradius = new LabImage(*labView, true);
             }
