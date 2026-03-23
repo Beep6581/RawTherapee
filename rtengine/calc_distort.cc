be4ad3699e977a8438fa5bccb508e545d52f0852
Daniel Gao <daniel.gao.work@gmail.com>
2026-03-22 22:21:09 -0400

    Move klt to external

--- rtengine/calc_distort.cc
+++ rtengine/calc_distort.cc
@@ -5,8 +5,8 @@ locations (before and after tracking) to text files and to PPM files,
 and prints the features to the screen.
 **********************************************************************/
 
-#include <pnmio.h>
-#include <klt.h>
+#include <klt/pnmio.h>
+#include <klt/klt.h>
 #include <cmath>
 #include <cstring>
 
