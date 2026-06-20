99bf4120b219b95f1b7123f7d221ac76e2a51f0e
Richard E Barber <kd6kxr@gmail.com>
2026-06-20 06:13:16 -0700

    mac: correct paths for fancy dmg background image

--- tools/osx/macosx_bundle.sh
+++ tools/osx/macosx_bundle.sh
@@ -436,10 +436,10 @@ function CreateDmg {
         echo "Building Fancy .dmg"
         touch message
         MESSAGE="$(cat message)"
-        magick ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png -pointsize 80 -fill Black -draw "text 14,1307 '${PROJECT_FULL_VERSION}'" -fill Salmon -draw "text 10,1300 '${PROJECT_FULL_VERSION}'" ./rtdmg-bkgd.png
-        magick ./rtdmg-bkgd.png -pointsize 90 -fill Black -gravity center -font Menlo-Bold -draw "text 5,120 \"$MESSAGE\"" -fill Red -gravity center -font Menlo-Bold -draw "text 1,124 \"$MESSAGE\"" ./rtdmg-bkgd.png
+        magick ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png -pointsize 80 -fill Black -draw "text 14,1307 '${PROJECT_FULL_VERSION}'" -fill Salmon -draw "text 10,1300 '${PROJECT_FULL_VERSION}'" ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png
+        magick ./rtdmg-bkgd.png -pointsize 90 -fill Black -gravity center -font Menlo-Bold -draw "text 5,120 \"$MESSAGE\"" -fill Red -gravity center -font Menlo-Bold -draw "text 1,124 \"$MESSAGE\"" ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png
         create-dmg \
-        --background ./rtdmg-bkgd.png \
+        --background ${PROJECT_SOURCE_DATA_DIR}/rtdmg-bkgd.png \
         --volname ${PROJECT_NAME}_${PROJECT_FULL_VERSION} \
         --volicon ${PROJECT_SOURCE_DATA_DIR}/rtdmg.icns \
         --window-pos 72 72 \
