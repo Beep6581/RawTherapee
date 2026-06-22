2fdb7a99c2c9f6ca94c05b4c0555d5bef836f785
Richard E Barber <kd6kxr@gmail.com>
2026-06-21 23:43:31 -0700

    mac: bug report webloc link includes template

--- tools/osx/macosx_bundle.sh
+++ tools/osx/macosx_bundle.sh
@@ -419,7 +419,7 @@ function CreateDmg {
     CreateWebloc       'Website' 'https://www.rawtherapee.com/'
     CreateWebloc 'Documentation' 'https://rawpedia.rawtherapee.com/'
     CreateWebloc         'Forum' 'https://discuss.pixls.us/c/software/rawtherapee'
-    CreateWebloc    'Report Bug' 'https://github.com/RawTherapee/RawTherapee/issues/new'
+    CreateWebloc    'Report Bug' 'https://github.com/RawTherapee/RawTherapee/issues/new?template=bug_report.md'
 
     # Disk image name
     if [[ -n $UNIVERSAL_URL ]]; then
